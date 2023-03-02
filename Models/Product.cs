299cae40283de69bf0deec634e43e9f90e2ca275
Aaron Warsop <86020594+aaronwarsop@users.noreply.github.com>
2023-03-02 12:02:32 +0000

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Shoebox.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string ProductImage { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string ProductImageAttribute { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,3 +6,4 @@ app.UseRouting();
 app.MapRazorPages();
 
 app.Run();
+
